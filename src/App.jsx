bc00b4294f983d5c2a30dc7594a2beefbf1921f3
Tarun Rajput <rajputtarun698@gmail.com>
2025-03-14 12:41:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="w-full h-full flex flex-col bg-gray-100">
+        <Navbar />
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <Paste />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes/:id",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <ViewPaste />
+      </div>
+    ),
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
